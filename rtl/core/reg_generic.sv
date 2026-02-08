4cbcaf17552ccfd394905d6183e6103e4e1801bd
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2026-02-08 05:48:45 -0300

    refactor: changed tabs into spaces

--- rtl/core/reg_generic.sv
+++ rtl/core/reg_generic.sv
@@ -1,18 +1,18 @@
 module reg_generic #(
-	parameter WIDTH = 8
+    parameter int WIDTH = 8
 ) (
-	input logic clk,
-	input logic rst,
-	input logic en,
-	input logic [WIDTH-1:0]data_in,
-	output logic [WIDTH-1:0]data_out
+    input logic clk,
+    input logic rst,
+    input logic en,
+    input logic [WIDTH-1:0] data_in,
+    output logic [WIDTH-1:0] data_out
 );
 
-always_ff @(posedge clk or posedge rst) begin
-	if (rst) begin 
-		data_out <= '0;
-	end else if(en) begin 
-		data_out <= data_in;
-	end
-end
+  always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+      data_out <= '0;
+    end else if (en) begin
+      data_out <= data_in;
+    end
+  end
 endmodule
--- tb/core/reg_generic_tb.sv
+++ tb/core/reg_generic_tb.sv
@@ -1,42 +1,44 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 module reg_generic_tb;
-	localparam TEST_WIDTH = 16;
-	
-	logic clk;
-	logic rst;
-	logic en;
-	logic [TEST_WIDTH - 1:0] data_in;
-	logic [TEST_WIDTH - 1:0] data_out;
+  localparam int TEST-WIDTH = 16;
 
-	reg_generic#(.WIDTH(TEST_WIDTH)) dut (
-		.clk(clk),
-		.rst(rst),
-		.en(en),
-		.data_in(data_in),
-		.data_out(data_out)
-		);
+  logic clk;
+  logic rst;
+  logic en;
+  logic [TEST-WIDTH - 1:0] data_in;
+  logic [TEST-WIDTH - 1:0] data_out;
 
-	initial begin
-		clk = 0;
-	end
-	always #5 clk = ~clk;
-	
-	initial begin
-		rst = 1;
-		en = 0;
-		data_in = 0;
+  reg_generic #(
+      .WIDTH(TEST-WIDTH)
+  ) dut (
+      .clk(clk),
+      .rst(rst),
+      .en(en),
+      .data_in(data_in),
+      .data_out(data_out)
+  );
 
-		#20 rst = 0;
-		#10 data_in = 16'hA5A5;
-		#10 en = 1;
-		#10 en = 0;
-		#10 data_in =16'hFFFF;
-		#50 $finish;
-	end
+  initial begin
+    clk = 0;
+  end
+  always #5 clk = ~clk;
 
-	initial begin 
-		$dumpfile("sim/reg_generic.vcd");
-		$dumpvars(0,reg_generic_tb);
-	end
-endmodule 
+  initial begin
+    rst = 1;
+    en = 0;
+    data_in = 0;
+
+    #20 rst = 0;
+    #10 data_in = 16'hA5A5;
+    #10 en = 1;
+    #10 en = 0;
+    #10 data_in = 16'hFFFF;
+    #50 $finish;
+  end
+
+  initial begin
+    $dumpfile("sim/reg_generic.vcd");
+    $dumpvars(0, reg_generic_tb);
+  end
+endmodule
 
