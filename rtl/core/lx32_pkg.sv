bb6e9682d4f2f231499ddefee5197a49894b7c05
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2026-02-09 19:30:08 -0300

    feat(isa): add base ALU operation definitions

---
+++ rtl/core/lx32_pkg.sv
@@ -0,0 +1,13 @@
+typedef enum logic [3:0] {
+    ALU_ADD,   // A + B
+    ALU_SUB,   // A - B
+    ALU_SLL,   // A << B
+    ALU_SLT,   // A < B (signed)
+    ALU_SLTU,  // A < B (unsigned)
+    ALU_XOR,   // A ^ B
+    ALU_SRL,   // A >> B (logical)
+    ALU_SRA,   // A >>> B (arithmetic)
+    ALU_OR,    // A | B
+    ALU_AND    // A & B
+} alu_op_e;
+
