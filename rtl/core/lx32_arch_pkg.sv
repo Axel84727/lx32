760a5c6f9ca43f2caeebcf190120488aa6a52098
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2026-02-11 16:10:34 -0300

    fix(arch): add OP_R_TYPE and export parameters for compiler visibility

--- rtl/core/lx32_arch_pkg.sv
+++ rtl/core/lx32_arch_pkg.sv
@@ -1,12 +1,14 @@
 package lx32_arch_pkg;
-  localparam [6:0] OP_IMM = 7'b0010011;
-  localparam [6:0] OP_LUI = 7'b0110111;
-  localparam [6:0] OP_AUIPC = 7'b0010111;
-  localparam [6:0] OP_STORE = 7'b0100011;
-  localparam [6:0] OP_LOAD = 7'b0000011;
-  localparam [6:0] OP_JAL = 7'b1101111;
-  localparam [6:0] OP_JALR = 7'b1100111;
-  localparam [6:0] OP_BRANCH = 7'b1100011;
+  parameter [6:0] OP_IMM = 7'b0010011;
+  parameter [6:0] OP_LUI = 7'b0110111;
+  parameter [6:0] OP_AUIPC = 7'b0010111;
+  parameter [6:0] OP_STORE = 7'b0100011;
+  parameter [6:0] OP_LOAD = 7'b0000011;
+  parameter [6:0] OP_JAL = 7'b1101111;
+  parameter [6:0] OP_JALR = 7'b1100111;
+  parameter [6:0] OP_BRANCH = 7'b1100011;
+  parameter [6:0] OP_R_TYPE = 7'b0110011;
+
 
   function automatic logic [31:0] get_i_imm(logic [31:0] i);
     return {{20{i[31]}}, i[31:20]};
