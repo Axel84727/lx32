a1fd96f26a8f0c0ea8cb8cdf3dabccc43792a90c
Axel Ibarrondo <135344010+Axel84727@users.noreply.github.com>
2026-04-01 17:08:04 -0300

    feat: added Subtarget in the script to generate an .inc file

--- tools/lx32_backend/TableGen/compile_td.sh
+++ tools/lx32_backend/TableGen/compile_td.sh
@@ -6,5 +6,6 @@ $TBLGEN -gen-dag-isel LX32.td -I $INCLUDE -I . -o LX32GenDAGISel.inc
 $TBLGEN -gen-callingconv LX32.td -I $INCLUDE -I . -o LX32GenCallingConv.inc
 $TBLGEN -gen-register-info LX32.td -I $INCLUDE -I . -o LX32GenRegisterInfo.inc
 $TBLGEN -gen-instr-info LX32.td -I $INCLUDE -I . -o LX32GenInstrInfo.inc
+$TBLGEN --gen-subtarget LX32.td -I $INCLUDE -I . -o LX32GenSubtargetInfo.inc
 
 echo "✅ TableGen: All .inc files were generated."
